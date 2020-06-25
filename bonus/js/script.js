329f59a0baae783b9ad9df7fe160bd2192f017fa
Michele Curtaz <mcurtaz@gmail.com>
2020-06-25 14:40:44 +0200

    bonus html + css sparaflash

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,16 +1,22 @@
 for (var i = 1; i <= 100; i++) {
 
   // (i % 3) da 0 per multipli di 3. 0 in booleano è falso. Quindi !(i % 3) è vero per i multipli di 3.
+  var print = 0;
 
   if ( !(i % 3) && !(i % 5) ){ // se il numero i è multiplo di 3 e 5
-    console.log("FizzBuzz");
+    print = "FizzBuzz";
   } else if (!(i % 3) && (i % 5)){ // se il numero i è multiplo di 3 ma non è multiplo di 5
-    console.log("Fizz");
+    print = "Fizz";
   } else if ((i % 3) && !(i % 5)){ // se il numero i non è multiplo di 3 ma è multiplo di 5
-    console.log("Buzz");
+    print = "Buzz";
   } else { // in tutti gli altri casi. Quindi non è multiplo di 3 e non è multiplo di 5
-    console.log(i);
+    print = i;
   }
 
+  if (print != i){
+    document.getElementById("print").innerHTML += '<li class="' + print + '">'+ print + '</li>';
+  } else{
+    document.getElementById("print").innerHTML += '<li>'+ print + '</li>';
+  }
 
 }
